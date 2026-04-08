bbbc9c0130c24c5eaf8e1ca744d46508a51a05b1
anacaumo <112983909+anacaumo@users.noreply.github.com>
2026-04-08 14:56:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -68,6 +68,19 @@ let remainingQuestions = allQuestions.slice();
 
 let currentQuestion = null;
 
+function shuffleArray(array) {
+  let shuffled = array.slice();
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = shuffled[i];
+    shuffled[i] = shuffled[j];
+    shuffled[j] = temp;
+  }
+
+  return shuffled;
+}
+
 function nextRound() {
 
   if (remainingQuestions.length === 0) {
@@ -87,7 +100,9 @@ function nextRound() {
   let optionsDiv = document.getElementById("options");
   optionsDiv.innerHTML = "";
 
-  currentQuestion.options.forEach(function(option) {
+  let shuffledOptions = shuffleArray(currentQuestion.options);
+
+shuffledOptions.forEach(function(option) {
     let btn = document.createElement("button");
     btn.className = "option";
     btn.innerText = option.text;
